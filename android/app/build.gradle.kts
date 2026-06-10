e0d4dd84631c3ecde687626a2e5d537030955b30
ZJL <1337455461@qq.com>
2026-06-10 23:48:37 +0800

    release(android): v1.2.0 接入公网域名 api.lzjyyds.top

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.buypilot"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
